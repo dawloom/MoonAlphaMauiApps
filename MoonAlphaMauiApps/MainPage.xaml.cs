f18d0939c51585754fa48217b60880f04c44164d
Haleem Khan <HaleemZahid@gmail.com>
2025-03-12 16:45:44 +0500

    added

--- MoonAlphaMauiApps/MainPage.xaml.cs
+++ MoonAlphaMauiApps/MainPage.xaml.cs
@@ -61,17 +61,7 @@ namespace MoonAlphaMauiApps
             this.ShowPopup(new CompletePopup());
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+      
 
         private void RadioButton_Checked(object sender, CheckedChangedEventArgs e)
         {
